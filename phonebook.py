e76f3e3abce7e30372deb34037d0621bb807c037
Kent Irvin <hondo189@gmail.com>
2018-10-06 19:38:00 -0500

    Phone# Regex Working

--- phonebook.py
+++ phonebook.py
@@ -5,7 +5,8 @@ Kent Irvin
 1001487672
 """
 #with file = open('data.txt', 'r+'):
-nameRegex = "^(O’)?([A-Za-z]{1,31})(-[A-Za-z]{1,31})?,?( (O’)?([A-Za-z]{1,31})(-[A-Za-z]{1,31})?)?$"
+nameRegex = "^(O’)?([A-Za-z]{1,31})(-[A-Za-z]{1,31})?,?( (O’)?([A-Za-z]{1,31})(-[A-Za-z]{1,31})?)?( (O’)?([A-Za-z]{1,31})(-[A-Za-z]{1,31})?)?$"
+phoneRegex = "[0-9]{5}.?([0-9]{5})?|(\+?[0-9]{1,3}[ \.\-]?)?\([0-9]{2,3}\)|[0-9]{3}[ \.\-][0-9]{4}|([0-9]{1,4}[ \.\-]){3,4}[0-9]{1,4}"
 buffer = input("ADD <Person> <Telephone #> \nDEL <Person> \nDEL <Telephone #> \nLIST \n")
 token = buffer.split()
 name = ""
@@ -21,13 +22,23 @@ for i in range(0, len(token)):
     else:
         phone = token[i]
 
+print("'"+action+"'")
+print("'"+name+"'")
+print("'"+phone+"'")   
+
 if action.upper() == "ADD":
     match = re.match(nameRegex, name)
     if match:
-        #phonebook.append([name,phone])
         print("VALID NAME")
+        match2 = re.match(phoneRegex, phone)
+        if match2:
+            #phonebook.append([name,phone])
+            print("VALID PHONE NUMBER")
+        else:
+            print("INVALID PHONE NUMBER")
     else:
         print("INVALID NAME")
+
 if action == "EXIT":
     exit()
-print(phonebook)
+#print(phonebook)
