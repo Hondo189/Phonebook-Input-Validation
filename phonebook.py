37acc5c820cf99b85a13f2750db38a0d87dd4ad2
Kent Irvin <hondo189@gmail.com>
2018-10-08 17:45:40 -0500

    FINISHED - All Functionality working

--- phonebook.py
+++ phonebook.py
@@ -9,9 +9,10 @@ Kent Irvin
 name = ""
 phonebook = []
 
+#import phonebook entries from phonebook.txt
 with open('phonebook.txt', 'r+') as file:
     for line in file:
-        print("Line: "+ line)
+        #print("Line: "+ line)
         token2 = line.split()
         #print(token2)
         for i in range(0, len(token2)):
@@ -26,29 +27,15 @@ with open('phonebook.txt', 'r+') as file:
                 #print(phone)
         phonebook.append([name,phone])
         name = ""
+
 print(phonebook)                
 nameRegex = "^(O’)?([A-Za-z]{1,31})(-[A-Za-z]{1,31})?,?( (O’)?([A-Za-z]{1,31})(-[A-Za-z]{1,31})?)?( (O’)?([A-Za-z]{1,31})(-[A-Za-z]{1,31})?)?$"
 phoneRegex = "[0-9]{5}.?([0-9]{5})?|(\+?[0-9]{1,3}[ \.\-]?)?\([0-9]{2,3}\)|[0-9]{3}[ \.\-][0-9]{4}|([0-9]{1,4}[ \.\-])[0-9]{1,4}"
 buffer = input("ADD <Person> <Telephone #> \nDEL <Person> \nDEL <Telephone #> \nLIST \nEXIT")
 token = buffer.split()
 action = token[0]
-"""
-for i in range(0, len(token)):
-    if i == 0:
-        action = token[i]
-    elif i == 1:
-        name += token[i]
-    elif i < len(token)-1:
-        name += " " + token[i]
-    else:
-        phone = token[i]
 
-print("'"+action+"'")
-print("'"+name+"'")
-print("'"+phone+"'")   
-"""
-
-if action.upper() == "ADD":
+if action.upper() == "ADD": #add new phonebook entry
     for i in range(1, len(token)):
         if i == 1:
             name += token[i]
@@ -59,48 +46,55 @@ if action.upper() == "ADD":
     
     match = re.match(nameRegex, name)
     if match:
-        print("VALID NAME")
+        #print("VALID NAME")
         match2 = re.match(phoneRegex, phone)
         if match2:
-            #phonebook.append([name,phone])
-            print("VALID PHONE NUMBER")
+            #print("VALID PHONE NUMBER")
             phonebook.append([name,phone])
         else:
-            print("INVALID PHONE NUMBER")
-            print("INVALID PHONE NUMBER", file-sys.stderr)
+            #print("INVALID PHONE NUMBER")
+            print("ERROR: Invalid Phone Number", file-sys.stderr)
             exit(1)
     else:
-        print("INVALID NAME")
-        print("INVALID NAME", file-sys.stderr)
+        #print("INVALID NAME")
+        print("Error: Invalid Name", file-sys.stderr)
         exit(1)
-elif action.upper() == "DEL":
+elif action.upper() == "DEL": #delete phonebook entry based on name or phonenumber
+    found = False
     for i in range(1, len(token)):
         if i == 1:
             temp = token[i]
         elif i < len(token):
             temp += " " + token[i]        
-    
-    print(temp)
-    if phonebook[1][1] == temp:
-        print(phonebook[1][1] +" = "+ temp)
-    if phonebook[1][1] != temp:
-        print(phonebook[1][1] +" != "+ temp)
-        
+            
     matchN = re.match(nameRegex, temp)
     matchP = re.match(phoneRegex, temp)
     if matchP:
         phonebook = [v for v in phonebook if v[1] != temp]
+        for i in range(0, len(phonebook)):
+                if phonebook[i][0] == temp:
+                    found = True
     elif matchN:
         phonebook = [v for v in phonebook if v[0].lower() != temp.lower()]
-elif action.upper() == "LIST":
+        for i in range(0, len(phonebook)):
+                if phonebook[i][1] == temp:
+                    found = True
+    else:
+        print("Error: Argument is not a valid name or phone number", file-sys.stderr)
+        exit(1)
+    if found == False: #if name/phone# unfound
+        print("Name/Phone Number not found")
+        exit(1)
+elif action.upper() == "LIST": #print phonebook
     for row in phonebook:
         for elem in row:
             print(elem, end=' ')
         print()
 elif action == "EXIT":
     exit()
-print(phonebook)
+#print(phonebook)
 
+#store phonebook entries into phonebook.txt
 with open('phonebook.txt', 'w+') as file:
     for row in phonebook:
         for elem in row:
